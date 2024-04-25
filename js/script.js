1ec73ae7ea8467e61d3ab2169b2b1bcb34d828c0
Tassio Luiz D. Carmo <tassiol.carmo@gmail.com>
2024-04-25 15:01:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function game(userChoice){
             lost(userChoice,computerChoice);
             break;
         default:
-            draw(userChoice,computerChoice); //MONSTA KADO
+            draw(userChoice,computerChoice);
             break;
     }
 
@@ -82,108 +82,3 @@ function draw(userChoice,computerChoice){
 
 
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function getComputerChoice(){
-    var randomNumberGenerator = Math.floor(Math.random() * 3) + 1
-    if(randomNumberGenerator == 1){
-        return 'Rock';
-    }
-    else if(randomNumberGenerator == 2){
-        return 'Paper';
-    }
-    else{
-        return 'Scissors';
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-
-    var result;
-
-    if(playerSelection === 'Paper' && computerSelection === 'Rock'){
-
-        return result = "Win";
-
-    }else if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
-        
-        return result = "Win";
-
-    }else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
-
-        return result = "Win";
-
-    }else if(playerSelection === 'Rock' && computerSelection === 'Paper'){
-
-        return result = "Lose";
-
-    }else if(playerSelection === 'Sciors' && computerSelection === 'Rock'){
-
-        return result = "Lose";
-
-    }else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
-
-        return result = "Lose";
-
-    }else {
-        return result = "Draw";
-    }
-
-}
-
-function game(){
-
-    let playerScore = 0;
-    let computerScore = 0;
-    
- for(var i = 0; i <= 4; i++){
-
-        const playerSelection = prompt("What's your Choice?")
-        const computerSelection = getComputerChoice();
-        const round =(playRound(playerSelection, computerSelection));
-
-        if(round === "Win"){
-            playerScore++;
-        }else if(round === "Lose"){
-            computerScore++;
-        }else{
-            playerScore++;
-            computerScore++;
-        }
-    } 
-    
-    if(playerScore > computerScore){
-        console.log("you Win your score:" + playerScore + "\nComputer Score:" + computerScore);
-    }else if(playerScore < computerScore){
-        console.log("you lose your score:" + playerScore + "\nComputer Score:" + computerScore);
-    }else{
-        console.log("it's a draw your score:" + playerScore + "\nComputer Score:" + computerScore);
-    }
-}
-*/
